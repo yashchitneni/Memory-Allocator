df9de1e75ae70de0db98dd193df335ccaa57633d
Yash Chitneni <yash.chitneni@gmail.com>
2014-02-17 18:24:11 -0600

    My_Malloc

--- my_allocator.c
+++ my_allocator.c
@@ -44,6 +44,10 @@ struct header{
     Addr next;
     char symbol;
     int size;
+    int LoR;
+//    int inheritance;
+//    int right_buddy;
+//    int left_buddy;
 };
 
 /*--------------------------------------------------------------------------*/
@@ -63,27 +67,36 @@ Addr increment_addr(Addr address, int sum) {
 }
 
 void create_Header(Addr current, int size, char symbol, Addr next) {
-    (header*)(current)->size = size;
-	(header*)(current)->symbol = symbol;
-	(header*)(current)->next = next;
+    header* temp = (header*)current;
+    temp->size = size;
+	temp->symbol = symbol;
+	temp->next = next;
 }
 
-unsigned int fib_at_index(unsigned int num) {
+void split_memory(unsigned int index) {
+    Addr split_left = free_list[index];
+    header* temp = (header*)split_left;
+    free_list[index] = temp->next;
+    unsigned int size = 0;
+    Addr split_right = increment_addr(split_left, size);
+    create_Header(split_right, size, 'H', free_list[index-1]);
+	create_Header(split_left, size, 'H', split_right);
+	free_list[index-1] = split_left;
+}
+
+unsigned int fib_at_index(unsigned int index) {
     
     fib[0] = 1;
     fib[1] = 2;
-    
     int i;
     
-    for (i = 2; i <= num; i++) {
+    for (i = 2; i <= index; i++) {
         fib[i] = fib[i-1] + fib[i-2];
     }
-    return fib[i];
+    return fib[i-1];
 }
 
 unsigned int largest_fib_index(unsigned int blocks) {
-    
-    // Sets the first two cases of fibonacci
     
     int fib[2] = {1, 2};
     int pos = 0;
@@ -91,63 +104,45 @@ unsigned int largest_fib_index(unsigned int blocks) {
     
     while (blocks > fib[pos]) {
         fib[pos] += fib[(pos+1) % 2];
-        pos++ % 2;
-        index++;
+        pos++;
+        pos %= 2;
+        if (blocks >= fib[pos]) {
+            index++;
+        }
     }
-    
     return index;
 }
 
-unsigned int largest_fib(unsigned int num) {
-    
-    // Sets the first two cases of fibonacci
-    
-    int fib[2] = {1, 2};
-    int pos = 0;
-    
-    while (num > fib[pos]) {
-        fib[pos] += fib[(pos+1) % 2];
-        pos++ % 2;
-    }
-    
-    // Returns the position that is not being pointed at
-    return fib[(pos + 1) % 2];
-} // largest_fib might be useless
-
 unsigned int init_allocator(unsigned int _basic_block_size, unsigned int _length) {
 
-        // Make sure length is properly divisible
-        if(_length % _basic_block_size != 0) {
-            _length += _length % _basic_block_size;
-        }
-        
-        memory = malloc(_length);
-        
-        //Find the largest fib index and then have the free_list point to the new object
-        
-        unsigned int free_list_size = largest_fib_index(_length/_basic_block_size);
-        free_list = new Addr[free_list_size];
-        
-        for (int i = 0; i < free_list_size; i++) {
-            free_list[i] = NULL;
-        }
-        
-        unsigned int sum = 0;
-        
-        while (_length > 0) {
-            unsigned int index = largest_fib_index(_length/_basic_block_size);
-            unsigned int block_amount = _basic_block_size * fib_at_index(index);
-            
-            Addr temp_addr = increment_addr(memory, sum);
-            free_list[index] = temp_addr;
-            
-            create_Header(free_list[index], block_amount, 'H', NULL);
-            
-            _length -= block_amount;
-            sum += block_amount;
-        }
-        return _length;
+    if(_length % _basic_block_size != 0) {
+        _length += _length % _basic_block_size;
+    }
 
+    memory = malloc(_length);
+    
+    unsigned int free_list_size = largest_fib_index(_length/_basic_block_size);
+    free_list = new Addr[free_list_size];
+        
+    for (int i = 0; i < free_list_size; i++) {
+        free_list[i] = NULL;
+    }
+    
+    unsigned int sum = 0;
+    
+    while (_length > 0) {
+        unsigned int index = largest_fib_index(_length/_basic_block_size);
+        unsigned int block_amount = _basic_block_size * fib_at_index(index);
+        
+        Addr temp_addr = increment_addr(memory, sum);
+        free_list[index] = temp_addr;
+        
+        create_Header(free_list[index], block_amount, 'H', NULL);
+        
+        _length -= block_amount;
+        sum += block_amount;
+    }
+    return _length;
 }
 
 extern Addr my_malloc(unsigned int _length) {
--- my_allocator.h
+++ my_allocator.h
@@ -8,6 +8,19 @@
  
  Modified:
  
+ # makefile
+ 
+ all: memtest
+ 
+ ackerman.o: ackerman.c ackerman.h
+ g++ -c -g ackerman.c
+ 
+ my_allocator.o : my_allocator.c my_allocator.h
+ g++ -c -g my_allocator.c
+ 
+ memtest: memtest.c ackerman.o my_allocator.o
+ g++ -o memtest memtest.c ackerman.o my_allocator.o
+ 
  */
 
 #ifndef _my_allocator_h_                   // include file only once
